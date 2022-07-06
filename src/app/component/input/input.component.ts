5f939ce132fe8bcca0c207e213b8cb88c41ad8b9
Joel Parks <joel.patrick.parks@gmail.com>
2022-07-06 10:00:04 -0400

    Updated Input
    Added controls and styles to Input component

--- src/app/component/input/input.component.ts
+++ src/app/component/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as wjcInput from '@grapecity/wijmo.input';
 
 @Component({
   selector: 'app-input',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
+  @ViewChild('filterAutoComplete', { static: true }) filterAutoComplete: wjcInput.AutoComplete;
+  countries = ['US', 'Germany', 'Mexico', 'Greece', 'Spain', 'China', 'Japan', 'Russia', 'UK', 'Finland', 'Norway'];
+  filteredCountries = ['US', 'Germany', 'Mexico', 'Greece', 'Spain'];
+  expandedCountries = [
+    { id: 0, country: 'US' },
+    { id: 1, country: 'Germany' },
+    { id: 2, country: 'Mexico' },
+    { id: 3, country: 'Greece' },
+    { id: 4, country: 'Spain' },
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  filterCheckboxChange(e: Event) {
+    if((<HTMLInputElement>e.target).checked) {
+      this.filterAutoComplete.itemsSource = this.filteredCountries;
+    } else {
+      this.filterAutoComplete.itemsSource = this.countries;
+    }
   }
 
 }
